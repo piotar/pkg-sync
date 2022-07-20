956dc2c578533dd9b65509f82b2cab7eb5e12112
piotar <piotar22@gmail.com>
2022-07-20 20:08:30 +0200

    Fixed randomHex

--- src/utils/randomHex.ts
+++ src/utils/randomHex.ts
@@ -1,3 +1,5 @@
 export function randomHex(): string {
-    return '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+    return (
+        '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0')
+    );
 }
